e8534e62c64c8b70700142998721ad115e8e3528
dariabrazhnyksuisse <daria.brazhnyk.suisse@gmail.com>
2021-01-04 16:49:59 +0100

    Jan-Fab filter -> index.value (date)

--- Merck_time_plot_2019.py
+++ Merck_time_plot_2019.py
@@ -11,12 +11,20 @@ register_matplotlib_converters()
 sns.set(font_scale=1.5, style="whitegrid")
 
 
-
-# Define relative path to file
+# The path to file
 file_path = ('TEST_FILE.csv')
 
-# Import file into pandas dataframe
-data_2019 = pd.read_csv(file_path)
+#date format
+date_format = '%Y-%d-%m %H:%M^%S'
+
+# To import the dates as a datetime object with parse_dates
+# index_col to set the index of the dataframe to the datetime values
+# Read csv file and Import file into pandas dataframe
+data_2019 = pd.read_csv(file_path,
+                            parse_dates=['date'], index_col=['date'])
+
+# Display the entire value
+print(data_2019.to_string())
 
 
 # Display first rows
@@ -25,30 +33,35 @@ data_2019.head()
 # View dataframe info
 data_2019.info()
 
+
+print("Index Values: ")
+print(data_2019.index.values)
+print("----------------------------------")
+# Subset data to Jan-Feb 2019
+power_jan_feb_2019 = data_2019['2019-01-01':'2019-02-28']
+power_jan_feb_2019.head()
+print("----------------------------------")
+
 # View column data types
 data_2019.dtypes
 
-# Check data type of first value in date column
-type(data_2019['date'][0])
-
-# Check data type of second value in time column
-type(data_2019['time'][1])
 
 # Create figure and plot space
-fig, ax = plt.subplots(figsize=(10, 10))
+fig, ax = plt.subplots(figsize=(8, 8))
 
-
-# Check data type of third value in power column
-type(data_2019['power'][2])
 
 # Add x-axis and y-axis
-ax.plot(data_2019['date'],
-        data_2019['power'],
+ax.plot(power_jan_feb_2019.index.values,
+        power_jan_feb_2019['power'],
         color='DeepSkyBlue')
 
 # Set title and labels for axes
 ax.set(xlabel="Date",
        ylabel="Power [kW]",
        title="Merck 2019")
+
+
+# Rotate tick marks on x-axis
+plt.setp(ax.get_xticklabels(), rotation=50)
 
 plt.show()

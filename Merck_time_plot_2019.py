ad9d33aaea3eda76ee13c09db2f496f4beb20fb1
dariabrazhnyksuisse <daria.brazhnyk.suisse@gmail.com>
2021-01-05 00:29:40 +0100

    test final

--- Merck_time_plot_2019.py
+++ Merck_time_plot_2019.py
@@ -9,14 +9,12 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Use white grid plot background from seaborn
-sns.set(font_scale=1, style="whitegrid")
+sns.set(font_scale=1.1, style="whitegrid", font='Calibri')
 
 
 # The path to file
 file_path = ('TEST_FILE.csv')
-
-#date format
-date_format = '%Y-%d-%m %H:%M^%S'
+file_path_2 = ('TEST_FILE_2.csv')
 
 # To import the dates as a datetime object with parse_dates
 # index_col to set the index of the dataframe to the datetime values
@@ -24,6 +22,10 @@ date_format = '%Y-%d-%m %H:%M^%S'
 data_2019 = pd.read_csv(file_path,
                             parse_dates=['date'], index_col=['date'])
 
+
+
+data_2019_2 = pd.read_csv(file_path_2,
+                            parse_dates=['date'], index_col=['date'])
 # Display the entire value
 print(data_2019.to_string())
 
@@ -38,41 +40,47 @@ print(data_2019.index.values)
 print("----------------------------------")
 # Subset data to Jan-Feb 2019
 power_jan_feb_2019 = data_2019['2019-01-01':'2019-02-28']
+power_jan_feb_2019_2 = data_2019_2['2019-01-01':'2019-02-28']
 power_jan_feb_2019.head()
 print("----------------------------------")
 
 # View column data types
 data_2019.dtypes
 
-#font common
-font = FontProperties()
-font.set_name('Calibri')
-
+# colors
+colOr = '#FFA500'
+colBl = 'DeepSkyBlue'
 
 # Create figure and plot space
-fig, ax = plt.subplots(figsize=(10, 10), tight_layout=True)
-
-#formatter for dates
-#formatter = power_jan_feb_2019.DateFormatter('%d-%m')
+fig, ax = plt.subplots(figsize=(12, 12), tight_layout=True)
 
 # Add x-axis and y-axis
 ax.plot(power_jan_feb_2019.index.values,
         power_jan_feb_2019['power'],
-        color='DeepSkyBlue', label='Nice Blue Line')
+        color = colBl, label='Initial power, kW')
 
 
-#try
+# Add x-axis and y-axis
+ax.plot(power_jan_feb_2019_2.index.values,
+        power_jan_feb_2019_2['power'],
+        color = colOr, label='Reduced power, kW')
+
+
+#signature for the color
 plt.grid(True)
-plt.legend(loc='upper right',prop={'size':12})
+plt.legend(loc='upper right',prop={'size':11})
 
+#to fill with colors
+plt.fill_between(power_jan_feb_2019.index.values, power_jan_feb_2019['power'], power_jan_feb_2019_2['power'], color=colBl)
+plt.fill_between(power_jan_feb_2019.index.values, power_jan_feb_2019_2['power'], 0, color=colOr)
 plt.axis(ymin=120, ymax=800)
 
 # Set title and labels for axes
-ax.set_xlabel("Date", fontproperties=font)
-ax.set_ylabel("Power [kW]", fontproperties=font)
-ax.set_title("Merck Jan-Feb 2019", fontsize='large', fontweight='bold', fontproperties=font)
+ax.set_xlabel("Date")
+ax.set_ylabel("Power [kW]")
+ax.set_title("Merck Jan-Feb 2019", fontsize='large', fontweight='bold')
 
-# Rotate tick marks on x-axis
+# Rotate tick marks on x-axips
 plt.setp(ax.get_xticklabels(), rotation=50)
 
 plt.show()

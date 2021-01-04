488ceef2b5fd396f7fdf47ad1e1cbc56fcce32fd
dariabrazhnyksuisse <daria.brazhnyk.suisse@gmail.com>
2021-01-04 19:02:45 +0100

    continue

--- Merck_time_plot_2019.py
+++ Merck_time_plot_2019.py
@@ -9,7 +9,7 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Use white grid plot background from seaborn
-sns.set(font_scale=1.5, style="whitegrid")
+sns.set(font_scale=1, style="whitegrid")
 
 
 # The path to file
@@ -33,7 +33,6 @@ data_2019.head()
 # View dataframe info
 data_2019.info()
 
-
 print("Index Values: ")
 print(data_2019.index.values)
 print("----------------------------------")
@@ -45,22 +44,30 @@ print("----------------------------------")
 # View column data types
 data_2019.dtypes
 
-
+#font common
 font = FontProperties()
 font.set_name('Calibri')
 
-# Create figure and plot space
-fig, ax = plt.subplots(figsize=(10, 10))
 
+# Create figure and plot space
+fig, ax = plt.subplots(figsize=(10, 10), tight_layout=True)
+
+#formatter for dates
+#formatter = power_jan_feb_2019.DateFormatter('%d-%m')
 
 # Add x-axis and y-axis
 ax.plot(power_jan_feb_2019.index.values,
         power_jan_feb_2019['power'],
-        color='DeepSkyBlue')
+        color='DeepSkyBlue', label='Nice Blue Line')
+
+
+#try
+plt.grid(True)
+plt.legend(loc='upper right',prop={'size':12})
+
+plt.axis(ymin=120, ymax=800)
 
 # Set title and labels for axes
-# ax.set_xlabel('time [s]', fontsize='large', fontweight='bold')
-
 ax.set_xlabel("Date", fontproperties=font)
 ax.set_ylabel("Power [kW]", fontproperties=font)
 ax.set_title("Merck Jan-Feb 2019", fontsize='large', fontweight='bold', fontproperties=font)
